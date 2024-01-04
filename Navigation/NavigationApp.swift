4dd7a9ffd6c079d637b3c74ff71c13d0a0fbbc3b
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2024-01-04 18:34:28 -0300

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Adriano Valumin on 04/01/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
